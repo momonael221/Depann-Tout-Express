ab72ccf4ce26b9150bd3eee77bfbeea646db3a69
Moussa Ball <moiseball20155@gmail.com>
2024-03-18 01:53:16 +0000

    Contenu Page Serrurerie Complete.

--- src/Pages/Serrurerie.tsx
+++ src/Pages/Serrurerie.tsx
@@ -1,6 +1,9 @@
 import {Breadcrumb, Button} from "flowbite-react";
 import {HiHome} from "react-icons/hi";
+
+import Verrou from "../assets/img/verrou.jpg";
 import Contact from "../assets/img/contact.jpg";
+import Serrurier from "../assets/img/serrurier.jpg";
 
 export default function Serrurerie() {
     return <>
@@ -21,22 +24,13 @@ export default function Serrurerie() {
                 <div className="grid grid-cols-1 gap-10">
                     <div className="col-span-1">
                         <div className="flex flex-col space-y-8">
-                            <h2 className={"text-cyan-700 text-3xl font-bold"}>Contactez-nous pour votre dépannage de
-                                serrurerie <span className="font-light">à Roubaix et alentour</span></h2>
+                            <h2 className={"text-cyan-700 text-3xl font-bold"}>Pour tout dépannage en serrurerie à Roubaix et ses environs, <span className="font-light">n'hésitez pas à nous contacter.</span></h2>
                         </div>
                     </div>
                     <div className="col-span-1 space-y-4">
-                        <p>Vous recherchez une entreprise qui sache installer et réparer tous types de serrures? Votre
-                            porte est bloquée ou a été forcée au cours d’un cambriolage? Afin de vous assurer une
-                            sécurité et une protection maximales, la société AAB Depann Tout Express prend en
-                            main <strong>vos travaux de serrurerie.</strong> En cas d'effraction ou de tentative
-                            d'effraction, il est essentiel de réparer votre serrure le plus rapidement possible pour
-                            garantir la sécurité de votre logement. Les serruriers de AAB Depann Tout Express à Roubaix
-                            sont également spécialisés dans la réparation de serrures suite à une effraction. Ils
-                            peuvent vous conseiller sur les meilleures options pour renforcer la sécurité de votre
-                            domicile et vous aider à installer des serrures plus robustes et plus résistantes aux
-                            tentatives de cambriolage. Nous nous déplaçons chez vous en urgence ou pour des
-                            interventions ponctuelles à Marcq-en-Barœul, Villeneuve-d'Ascq, Lille et Tourcoing.</p>
+                        <p>
+                            À la recherche d'une entreprise capable d'installer et de réparer tous types de serrures ? Que votre porte soit bloquée ou qu'elle ait été forcée lors d'un cambriolage, la société Depann Tout Express prend en charge vos travaux de serrurerie pour garantir votre sécurité et votre protection. En cas d'effraction ou de tentative, il est crucial de réparer votre serrure rapidement afin de sécuriser votre domicile. Les serruriers de Depann Tout Express à Roubaix sont spécialisés dans la réparation de serrures après une effraction et peuvent vous conseiller sur les meilleures options pour renforcer la sécurité de votre domicile en installant des serrures plus robustes et résistantes aux tentatives de cambriolage. Nous intervenons en urgence ou sur rendez-vous à Marcq-en-Barœul, Villeneuve-d'Ascq, Lille et Tourcoing.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -47,22 +41,14 @@ export default function Serrurerie() {
                 <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-32">
                     <div className={"col-span-1"}>
                         <div className="flex flex-col space-y-8">
-                            <h2 className={"text-cyan-700 text-3xl font-bold"}>Besoin d'un serrurier pour poser une
-                                serrure ou un verrou ?</h2>
-                            <p className={"text-lg"}>Si vous êtes à la recherche d'un dépannage de serrure ou d'une
-                                ouverture de porte claquée à Roubaix, vous avez de la chance! L'entreprise AAB Depann
-                                Tout Express a les compétents et qui pourront vous aider dans ces situations délicates.
-                                Votre cylindre de serrure est grippé? Vous aimeriez changer le barillet de votre porte?
-                                Nos experts vous accompagnent et assurent entre autres la pose de serrures A2P (serrures
-                                trois points et serrures cinq points).</p>
-                            <p className={"text-lg"}>Nous installons également des verrous haute sécurité et
-                                des <strong>portes blindées</strong>, garanties pour leur véritable résistance face aux
-                                tentatives d’effraction.</p>
-                            <Button className={"max-w-sm"} size={"xl"} pill outline>Pose de serrure</Button>
+                            <h2 className={"text-cyan-700 text-3xl font-bold"}>À la recherche d'un serrurier pour l'installation d'une serrure ou d'un verrou ?</h2>
+                            <p className={"text-lg"}>Si vous avez besoin d'un dépannage de serrure ou d'une ouverture de porte claquée à Roubaix, vous êtes au bon endroit ! L'entreprise Depann Tout Express dispose de professionnels compétents prêts à vous aider dans ces situations délicates. Que votre cylindre de serrure soit grippé ou que vous souhaitiez remplacer le barillet de votre porte, nos experts sont là pour vous accompagner. Ils assurent notamment la pose de serrures A2P, qu'il s'agisse de serrures trois points ou cinq points.</p>
+                            <p className={"text-lg"}>Nous proposons également l'installation de verrous haute sécurité ainsi que de <strong>portes blindées</strong>, assurant une véritable résistance contre les tentatives d'effraction.</p>
+                            <Button href={"/pose-serrure"} className={"max-w-sm"} size={"xl"} pill outline>Pose de serrure</Button>
                         </div>
                     </div>
                     <div className={"col-span-1"}>
-                        <img className={"rounded-lg"} src={Contact} alt={Contact}/>
+                        <img className={"w-full max-h-[400px] object-cover rounded-lg"} src={Verrou} alt={Verrou}/>
                     </div>
                 </div>
             </div>
@@ -72,24 +58,17 @@ export default function Serrurerie() {
             <div className="container mx-auto">
                 <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-32">
                     <div className={"col-span-1"}>
-                        <img className={"rounded-lg"} src={Contact} alt={Contact}/>
+                        <img className={"w-full max-h-[400px] object-cover rounded-lg"} src={Contact} alt={Contact}/>
                     </div>
                     <div className={"col-span-1"}>
                         <div className="flex flex-col space-y-8">
-                            <h2 className={"text-cyan-700 text-3xl font-bold"}>Dépannage de serrurerie en urgence: <span
-                                className="font-light">nous sommes à votre écoute</span></h2>
-                            <p>Vous avez perdu vos clés ou êtes victime d’un cambriolage? La porte de votre maison a
-                                claqué et vous vous retrouvez dehors? N’attendez pas pour nous appeler!</p>
-                            <p>Notre serrurier dispose des outils et des compétences nécessaires pour accéder à votre
-                                domicile rapidement et en toute sécurité, sans causer de dommages inutiles à votre porte
-                                ou à votre serrure.</p>
-                            <p>Disponible 7j/7 et 24h/24, un serrurier peut intervenir dans l’heure pour votre dépannage
-                                dans un rayon d’environ 60 km autour de la ville de Roubaix, Lille et tout le
-                                département du Nord .</p>
-                            <p>Dans le cas où la destruction de votre serrure est nécessaire, nous installons un système
-                                de fermeture solide et provisoire en attendant de procéder au remplacement définitif de
-                                votre serrure.</p>
-                            <Button className={"max-w-sm"} size={"xl"} pill outline>Dépannage de serrurerie</Button>
+                            <h2 className={"text-cyan-700 text-3xl font-bold"}>Urgence serrurerie: <span
+                                className="font-light">nous sommes là pour vous aider.</span></h2>
+                            <p>Si vous avez égaré vos clés ou êtes victime d'un cambriolage, ou si la porte de votre maison s'est refermée et vous vous retrouvez à l'extérieur, ne tardez pas à nous contacter !</p>
+                            <p>Notre serrurier est équipé des outils et possède les compétences requises pour accéder à votre domicile rapidement et en toute sécurité, sans endommager inutilement votre porte ou votre serrure.</p>
+                            <p>Disponible 24h/24 et 7j/7, notre serrurier peut intervenir dans l'heure pour vos urgences de dépannage dans un rayon d'environ 60 km autour des villes de Roubaix, Lille, et dans tout le département du Nord.</p>
+                            <p>Si la destruction de votre serrure est inévitable, nous mettons en place un dispositif de fermeture robuste et temporaire en attendant de procéder au remplacement permanent de votre serrure.</p>
+                            <Button href={"/isolateur-interieure"} className={"max-w-sm"} size={"xl"} pill outline>Dépannage de serrurerie</Button>
                         </div>
                     </div>
                 </div>
@@ -101,14 +80,14 @@ export default function Serrurerie() {
                 <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-32">
                     <div className={"col-span-1"}>
                         <div className="flex flex-col space-y-8">
-                            <h2 className={"text-cyan-700 text-3xl font-bold"}>Notre serrurier intervient rapidement <span className="font-light">pour le dépannage de votre serrure</span></h2>
-                            <p className={"text-lg"}>Vous avez besoin de conseils pour sécuriser votre habitat? Vous aimeriez programmer un rendez-vous rapidement? Contactez-nous par téléphone ou par e-mail grâce à notre formulaire de contact.</p>
-                            <p className={"text-lg"}>Nous intervenons dans le respect des délais prévus. Renseignez-vous concernant nos facilités de paiement (paiement en plusieurs fois sans frais, par exemple).</p>
-                            <Button className={"max-w-sm"} size={"xl"} pill outline>Contactez-nous</Button>
+                            <h2 className={"text-cyan-700 text-3xl font-bold"}>Notre serrurier se rend rapidement disponible pour assurer <span className="font-light">le dépannage de votre serrure</span></h2>
+                            <p className={"text-lg"}>Besoin de conseils pour renforcer la sécurité de votre domicile ? Envie de fixer un rendez-vous sans délai ? Contactez-nous par téléphone ou par e-mail via notre formulaire de contact.</p>
+                            <p className={"text-lg"}>Nous respectons les délais convenus pour nos interventions. Découvrez nos options de paiement flexibles, comme le paiement en plusieurs fois sans frais.</p>
+                            <Button href={"/contact"} className={"max-w-sm"} size={"xl"} pill outline>Contactez-nous</Button>
                         </div>
                     </div>
                     <div className={"col-span-1"}>
-                        <img className={"rounded-lg"} src={Contact} alt={Contact}/>
+                        <img className={"rounded-lg"} src={Serrurier} alt={Serrurier}/>
                     </div>
                 </div>
             </div>
