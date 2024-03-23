bc90e6e415564dac0b5042753b4b3c48f6929f32
Moussa Ball <moiseball20155@gmail.com>
2024-03-23 20:42:42 +0000

    Fix Footer Responsiveness.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,54 +14,91 @@ export default function Footer() {
         <FooterLayout className="rounded-none border-t border-gray-100 dark:border-gray-700" container>
             <div className="w-full container mx-auto rounded-none">
                 <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-                    <div className="flex flex-col space-y-4">
-                        <FooterLayout.Brand href="/" src="/logo.jpg" alt="Depann-Tout-Express Logo"/>
-                        <span className="text-gray-500 dark:text-white mb-4">Disponibilité: 24h/24 - 7j/7</span>
-                        <div className="flex flex-wrap gap-2">
-                            <Button href={"/contact"} size={"xl"} pill outline>Contactez-nous</Button>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-8 ju sm:mt-4 md:grid-cols-2 lg:grid-cols-4 sm:gap-6">
+                        <div className="flex flex-col space-y-4">
+                            <FooterLayout.Brand href="/" src="/logo.jpg" alt="Depann-Tout-Express Logo"/>
+                            <span className="text-gray-500 dark:text-white mb-4">Disponibilité: 24h/24 - 7j/7</span>
+                            <div className="flex flex-wrap gap-2">
+                                <Button href={"/contact"} size={"xl"} pill outline>Contactez-nous</Button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="grid grid-cols-1 gap-8 mt-8 sm:mt-4 md:grid-cols-3 sm:gap-6">
                         <div>
                             <FooterLayout.Title title="Siège social"/>
                             <FooterLayout.LinkGroup col>
-                                <FooterLayout.Link href="#"><div className="flex items-center space-x-1"><BsHouse/> <span>198 Rue de l'Alma 59100 Roubaix</span></div></FooterLayout.Link>
-                                <FooterLayout.Link href="#"><div className="flex items-center space-x-1"><BsPhone/> <span>Téléphone Portable</span></div></FooterLayout.Link>
-                                <FooterLayout.Link href="#"><div className="flex items-center space-x-1"><BsTelephone/> <span>Téléphone Fixe</span></div></FooterLayout.Link>
-                                <FooterLayout.Link href="#"><div className="flex items-center space-x-1"><BsEnvelope/> <span>Envoyez-nous un e-mail</span></div></FooterLayout.Link>
+                                <FooterLayout.Link href="/contact">
+                                    <div className="flex items-center space-x-2">
+                                        <div className={"W-4 h-4"}><BsHouse/></div>
+                                        <span>198 Rue de l'Alma 59100 Roubaix</span>
+                                    </div>
+                                </FooterLayout.Link>
+                                <FooterLayout.Link href="tel:+33 06 25 11 12 81">
+                                    <div className="flex items-center space-x-2">
+                                        <div className={"W-4 h-4"}><BsPhone/></div>
+                                        <span>+33 06 25 11 12 81</span></div>
+                                </FooterLayout.Link>
+                                <FooterLayout.Link href="tel:+33 03 20 84 38 99">
+                                    <div className="flex items-center space-x-2">
+                                        <div className={"W-4 h-4"}><BsTelephone/></div>
+                                        <span>+33 03 20 84 38 99</span></div>
+                                </FooterLayout.Link>
+                                <FooterLayout.Link href="mailto:depanntoutexpress@hotmail.fr">
+                                    <div className="flex items-center space-x-2">
+                                        <div className={"W-4 h-4"}><BsEnvelope/></div>
+                                        <span>Envoyez-nous un e-mail</span>
+                                    </div>
+                                </FooterLayout.Link>
                             </FooterLayout.LinkGroup>
                         </div>
                         <div>
                             <FooterLayout.Title title="Nos agences"/>
                             <FooterLayout.LinkGroup col>
-                                <FooterLayout.Link href="#"><div className="flex items-center space-x-1"><BsHouse/><span>168 rue Philadelphie – 59800 Lille</span></div></FooterLayout.Link>
-                                <FooterLayout.Link href="#"><div className="flex items-center space-x-1"><BsPhone/><span>Téléphone</span></div></FooterLayout.Link>
-                                <FooterLayout.Link href="#"><div className="flex items-center space-x-1"><BsHouse/><span>25 avenue Saint-Amant - 59300 Valencienne</span></div></FooterLayout.Link>
-                                <FooterLayout.Link href="#"><div className="flex items-center space-x-1"><BsPhone/><span>Téléphone</span></div></FooterLayout.Link>
+                                <FooterLayout.Link href="/contact">
+                                    <div className="flex items-center space-x-2">
+                                        <div className={"W-4 h-4"}><BsHouse/></div>
+                                        <span>168 rue Philadelphie – 59800 Lille</span>
+                                    </div>
+                                </FooterLayout.Link>
+                                <FooterLayout.Link href="tel:+33 06 25 11 99 95">
+                                    <div className="flex items-center space-x-2">
+                                        <div className={"W-4 h-4"}><BsTelephone/></div>
+                                        <span>+33 06 25 11 99 95</span></div>
+                                </FooterLayout.Link>
+                                <FooterLayout.Link href="/contact">
+                                    <div className="flex items-center space-x-2">
+                                        <div className={"W-4 h-4"}><BsHouse/></div>
+                                        <span>25 avenue Saint-Amant - 59300 Valencienne</span>
+                                    </div>
+                                </FooterLayout.Link>
+                                <FooterLayout.Link href="tel:+33 07 53 44 54 40">
+                                    <div className="flex items-center space-x-2">
+                                        <div className={"W-4 h-4"}><BsTelephone/></div>
+                                        <span>+33 07 53 44 54 40</span>
+                                    </div>
+                                </FooterLayout.Link>
                             </FooterLayout.LinkGroup>
                         </div>
                         <div>
                             <FooterLayout.Title title="Plan du site"/>
                             <FooterLayout.LinkGroup col>
-                                <FooterLayout.Link href="#">Accueil</FooterLayout.Link>
-                                <FooterLayout.Link href="#">À propos</FooterLayout.Link>
-                                <FooterLayout.Link href="#">Serrurerie</FooterLayout.Link>
-                                <FooterLayout.Link href="#">Travaux de rénovation</FooterLayout.Link>
-                                <FooterLayout.Link href="#">Électricité</FooterLayout.Link>
-                                <FooterLayout.Link href="#">Plomberie</FooterLayout.Link>
-                                <FooterLayout.Link href="#">Isolation intérieure</FooterLayout.Link>
-                                <FooterLayout.Link href="#">Contact</FooterLayout.Link>
+                                <FooterLayout.Link href="/">Accueil</FooterLayout.Link>
+                                <FooterLayout.Link href="/about">À propos</FooterLayout.Link>
+                                <FooterLayout.Link href="/serrurerie">Serrurerie</FooterLayout.Link>
+                                <FooterLayout.Link href="/travaux-de-renovation">Travaux de rénovation</FooterLayout.Link>
+                                <FooterLayout.Link href="/electricite">Électricité</FooterLayout.Link>
+                                <FooterLayout.Link href="/reparation-plomberie">Plomberie</FooterLayout.Link>
+                                <FooterLayout.Link href="/isolateur-interieure">Isolation intérieure</FooterLayout.Link>
+                                <FooterLayout.Link href="/contact">Contact</FooterLayout.Link>
                             </FooterLayout.LinkGroup>
                         </div>
                     </div>
                 </div>
                 <FooterLayout.Divider/>
                 <div className="w-full sm:flex sm:items-center sm:justify-between">
-                    <FooterLayout.Copyright href="#" by="DEPANN TOUT EXPRESS™" year={new Date().getFullYear()}/>
+                    <FooterLayout.Copyright href="/" by="DEPANN TOUT EXPRESS™" year={new Date().getFullYear()}/>
                     <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-                        <FooterLayout.Icon href="#" icon={BsFacebook} />
-                        <FooterLayout.Icon href="#" icon={BsInstagram} />
-                        <FooterLayout.Icon href="#" icon={BsTwitter} />
+                        <FooterLayout.Icon href="#" icon={BsFacebook}/>
+                        <FooterLayout.Icon href="#" icon={BsInstagram}/>
+                        <FooterLayout.Icon href="#" icon={BsTwitter}/>
                     </div>
                 </div>
             </div>
